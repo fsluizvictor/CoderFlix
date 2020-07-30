0344564630c596d650b7417b78062be378da22ed
fsluizvictor <fs.luizvictor@gmail.com>
2020-07-30 00:39:21 -0300

    desenvolvendo carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import Menu from './components/Menu'
+import BannerMain from './components/BannerMain'
+import dadosIniciais from './data/dados_iniciais.json'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div >
-      <Menu/>
+      <Menu />
+
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"cçlkasncçlakcnçalscknascçlkn"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
     </div>
   );
 }
