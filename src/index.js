e83abc2e185382919ce4b2cfccb5896194647df1
fsluizvictor <fs.luizvictor@gmail.com>
2020-07-30 23:33:46 -0300

    fazendo rotas das páginas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/Video'
+import Home from './pages/Home';
+import Error from './pages/Error'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Error} />
+      <Home />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
+ 
---
+++ src/pages/Error/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+import error404 from '../../assets/error404.png'
+
+function Error() {
+    return (
+        <div>
+            <img src={error404} alt="Error 404" />
+        </div>
+    )
+
+}
+
+export default Error 
