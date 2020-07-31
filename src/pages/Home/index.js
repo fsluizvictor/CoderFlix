3535f86d17bc8aa8baa72e37b3f8ce2a0e7c7fc8
fsluizvictor <fs.luizvictor@gmail.com>
2020-07-31 01:03:11 -0300

    adicionado carousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import Menu from '../../components/Menu'
-import BannerMain from '../../components/BannerMain'
-import dadosIniciais from '../../data/dados_iniciais.json'
-import Carousel from '../../components/Carousel'
-import Footer from '../../components/Footer'
-
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
-    <div >
+    <div style={{ background: "#141414" }}>
       <Menu />
-
 
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"cçlkasncçlakcnçalscknascçlkn"}
+        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
       />
 
       <Carousel
@@ -24,25 +22,26 @@ function Home() {
       />
 
       <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
+        category={dadosIniciais.categorias[1]}
       />
 
       <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+        category={dadosIniciais.categorias[2]}
+      />      
 
       <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+        category={dadosIniciais.categorias[3]}
+      />      
 
       <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+        category={dadosIniciais.categorias[4]}
+      />      
 
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
