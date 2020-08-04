b95087e1c06da94e2fdb04f9cf70ad57b2aae1a2
fsluizvictor <fs.luizvictor@gmail.com>
2020-08-04 16:14:49 -0300

    configurando home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+//import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 import categoriaRepository from '../../repositories/categorias';
+import PageDefault from '../../components/PageDefault';
 
 function Home() {
+
+  const [dadosIniciais, setDadosIniciais] = useState([])
+
   useEffect(() => {
     categoriaRepository.getAllWithVideos()
       .then((categoriaComVideos) => {
-        console.log('çjwefkljwnef')
-        console.log(categoriaComVideos);
-      });
+        setDadosIniciais(categoriaComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
   }, []);
 
   return (
-    <div style={{ background: '#141414' }}>
+    <PageDefault>
       <Menu />
 
-      <BannerMain
+      {JSON.stringify(dadosIniciais)}
+
+      {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
-      />
+      /> */}
 
-      <Carousel
+      {/* <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
@@ -48,10 +56,9 @@ function Home() {
 
       <Carousel
         category={dadosIniciais.categorias[5]}
-      />
+      /> */}
 
-      <Footer />
-    </div>
+    </PageDefault>
   );
 }
 
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -3,10 +3,14 @@ import config from '../config';
 const URL_CATEGORIES = `${config.URL_BACKEND}/categorias?_embed=videos`;
 
 function getAllWithVideos() {
-  return fetch(URL_CATEGORIES)
+  return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (responseServer) => {
-      const response = await responseServer.json();
-      return response;
+      if (responseServer.ok) {
+        const response = await responseServer.json();
+        return response;
+      }
+
+      throw new Error('Não foi ppossível pegar os dados');
     });
 }
 
