25b12652b0c27622aff01c0aac1642bbc0c1e244
fsluizvictor <fs.luizvictor@gmail.com>
2020-08-04 00:14:21 -0300

    adicionando vídeos ao repositório

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 function ButtonLink(props) {
-    //props => { classname="o que alguém passar", href="/" }    
+    // props => { classname="o que alguém passar", href="/" }
     return (
         <a href={props.href} className={props.className}>
             {props.children}
         </a>
-    )
+    );
 }
 
-export default ButtonLink
+export default ButtonLink;
